959839ac3992351578a89907e384eec690a6801d
MatthewJLynch <matthewlynch1996@gmail.com>
2017-10-12 10:52:44 +0000

    update to python

--- catalogue/index.py
+++ catalogue/index.py
@@ -11,5 +11,11 @@ def index():
 def berlin():
   return render_template('berlin.html', title='Berlin', berlin=berlin)
 
+#404 Page
+@app.errorhandler(404)
+def page_not_found(error):
+  return render_template('404.html', title='Page Not Found',
+  page_not_found=page_not_found)
+
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
